44fd9223ef27fd1fe5bdb7dcd3b9acc871102d16
Burdinova <91088809+Burdinova@users.noreply.github.com>
2021-09-25 22:10:10 +0300

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    double a, b, c;
+    cout << "Координата А=";
+    cin >> a;
+    cout << "Координата B=";
+    cin >> b;
+    cout << "Координата C=";
+    cin >> c;
+    cout << "АC=" << (c - a) <<endl;
+    cout << "BC=" <<  (c - b) <<endl;
+    cout << "АC+BC=" << (c - a) +(c - b);
+    return 0;
+}
+
---
+++ 4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	double x1, x2, y1, y2;
+	cout << "Координата точки А:";
+	cout << " x1=";
+	cin >> x1;
+	cout << " y1=";
+	cin >> y1;
+	cout << "Координата точки C:";
+	cout << " x2=";
+	cin >> x2;
+	cout << " y2=";
+	cin >> y2;
+	cout << "Периметр=" << 2 * ((x2 - x1) + (y2 - y1)) <<endl;
+	cout << "Площадь=" << ((x2 - x1) * (y2 - y1));
+	return 0;
+}
+
