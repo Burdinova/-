02a848e2c5892b6190d9248c4b71169a550b35c3
Burdinova <91088809+Burdinova@users.noreply.github.com>
2021-10-14 21:28:13 +0300

    Add files via upload

---
+++ laba 9/4^.cpp
@@ -0,0 +1,27 @@
+﻿/* 4. Даны целые положительные числа A, B, C.На прямоугольнике размера A × B размещено максимально
+возможное количество квадратов со стороной C (без наложений). Найти количество квадратов, размещенных 
+на прямоугольнике, а также площадь незанятой части прямоугольника.*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int a, b, c;
+    cout << "Введите размеры прямоугольника" << endl;
+    cout << "A=";
+    cin >> a;
+    cout << "B=";
+    cin >> b;
+    cout << "Введите сторону квадрата" << endl;
+    cout << "C=";
+    cin >> c;
+    double col_a = floor(a / c);
+    double col_b = floor(b / c);
+    int col_kv = col_a * col_b;
+    int s_kv = col_kv * c * c;
+    int ost = a * b - s_kv;
+    cout << "Количество квадратов=" << col_kv <<endl;
+    cout << "Площадь незанятой части=" << ost;
+    return 0;
+}
---
+++ laba 9/5^.cpp
@@ -0,0 +1,15 @@
+﻿/*5. Дан номер некоторого года (целое положительное число). Определить соответствующий ему номер столетия,
+учитывая, что, к примеру, началом 20 столетия был 1901 год*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    double god, stol;
+    cout << "Введите год" << endl << "god=";
+    cin >> god;
+    stol = ceil(god / 100);
+    cout << "Номер столетия = " << stol;
+    return 0;
+}
