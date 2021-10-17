e0dacad29e3912d5a14360b4111bca5ffee655b4
Burdinova <91088809+Burdinova@users.noreply.github.com>
2021-10-17 09:46:59 +0300

    Add files via upload

---
+++ laba 11/2)...cpp
@@ -0,0 +1,34 @@
+﻿/*2. Даны три числа. Найти сумму двух наибольших из них*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    double a, b,c,z;
+    cout << "Найти сумму двух наибольших чисел "<< endl;
+    cout << "Введите А = ";
+    cin >> a;
+    cout << "Введите B = ";
+    cin >> b;
+    cout << "Введите C = ";
+    cin >> c;
+    if ((a>=b) && (c>=b))
+    {
+        z = a + c;
+        cout << "Ответ= "<<z;
+    }
+    else {
+        if ((b >= a) && (c >= a))
+        {
+            z = c + b;
+            cout << "Ответ= " << z;
+        }
+        else
+        {
+            z = a + b;
+            cout << "Ответ= " << z;
+        }
+    }
+    return 0;
+}
