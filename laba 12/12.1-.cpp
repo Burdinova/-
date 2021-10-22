f348d10e9f8fe5c9ed4b91e4f4d2ab52b22edbe3
Burdinova <91088809+Burdinova@users.noreply.github.com>
2021-10-22 10:43:30 +0300

    Add files via upload

---
+++ laba 12/12.1-.cpp
@@ -0,0 +1,82 @@
+﻿/* 1. Дан номер дня – целое число от 1 до 31 и месяца — целое число в диапазоне 1–12
+(1 — январь, 2 — февраль и т. д.). Вывести дату в виде текста (например, «пятое января»).*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x, d, a, y;
+    setlocale(LC_ALL, "Rus");
+    cout << "Вывести дату в виде текста"<<endl;
+    cout << "Введите номер дня: ";
+    cin >> x;
+    cout << "Введите номер месяца: ";
+    cin >> y;
+    if (x > 31) cout << "На нашей планете нет такого числа, но у марсиан оно определённо существует!";
+    else {
+        d = x % 10; //смотрим единицы
+        a = x / 10; // смотрим десятки
+        if (((x < 10) || (x > 20)) && (x != 30))
+        {
+            switch (a)
+            {
+            case 2: cout << "двадцать "; break;
+            case 3: cout << "тридцать "; break;
+            default:
+                break;
+            }
+            switch (d)
+            {
+            case 1: cout << "первое "; break;
+            case 2: cout << "второе "; break;
+            case 3: cout << "третье "; break;
+            case 4: cout << "четвертое "; break;
+            case 5: cout << "пятое "; break;
+            case 6: cout << "шестое "; break;
+            case 7: cout << "седьмое "; break;
+            case 8: cout << "восьмое "; break;
+            case 9: cout << "девятое "; break;
+            default:
+                break;
+            }
+        }
+        else
+        {
+            switch (x)
+            {
+            case 10: cout << "десятое "; break;
+            case 11: cout << "одиннадцатое "; break;
+            case 12: cout << "двенадцатое "; break;
+            case 13: cout << "тринадцатое "; break;
+            case 14: cout << "четырнадцатое "; break;
+            case 15: cout << "пятнадцатое "; break;
+            case 16: cout << "шетнадцатое "; break;
+            case 17: cout << "семнадцатое "; break;
+            case 18: cout << "восемнадцатое "; break;
+            case 19: cout << "девятнадцатое "; break;
+            case 20: cout << "двадцатое "; break;
+            case 30: cout << "тридцатое "; break;
+            default:
+                break;
+            }
+        }
+        switch (y)
+        {
+        case 1: cout << "января"; break;
+        case 2: cout << "февраля"; break;
+        case 3: cout << "марта"; break;
+        case 4: cout << "апреля"; break;
+        case 5: cout << "мая"; break;
+        case 6: cout << "июня"; break;
+        case 7: cout << "июля"; break;
+        case 8: cout << "августа"; break;
+        case 9: cout << "сентября"; break;
+        case 10: cout << "октября"; break;
+        case 11: cout << "ноября"; break;
+        case 12: cout << "декабря"; break;
+        default: cout << "На нашей планете нет такого месяца, но у марсиан он определённо существует";
+            break;
+        }
+    }
+    return 0;
+}
---
+++ laba 12/12.4.cpp
@@ -0,0 +1,82 @@
+﻿/*4. Дано целое число в диапазоне 100–999. Вывести строку-описание данного числа, например:
+256 — «двести пятьдесят шесть», 814 — «восемьсот четырнадцать».*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x, d, a,z, y;
+    setlocale(LC_ALL, "Rus");
+    cout << "Введите число в диапазоне 100–999: ";
+    cin >> x;
+    if ((x < 100) || (x > 999)) cout << "Число выходит за рамки диапазона";
+    else {
+        y = x / 100; //смотрим сотни
+        d = x % 10; //смотрим единицы
+        a = (x / 10)%10; // смотрим десятки
+        z = x % 100; //два последних числа
+        switch (y)
+        {
+        case 1: cout << "сто "; break;
+        case 2: cout << "двести "; break;
+        case 3: cout << "триста "; break;
+        case 4: cout << "четыреста "; break;
+        case 5: cout << "пятьсот "; break;
+        case 6: cout << "шестьсот "; break;        
+        case 7: cout << "семьсот "; break;
+        case 8: cout << "восемьсот "; break;
+        case 9: cout << "девятьсот "; break;
+
+        default:
+            break;
+        }
+        if ((z < 11) || (z > 19)) {
+            switch (a)
+            {
+            case 1: cout << "десять "; break;
+            case 2: cout << "двадцать "; break;
+            case 3: cout << "тридцать "; break;
+            case 4: cout << "сорок "; break;
+            case 5: cout << "пятьдесят "; break;
+            case 6: cout << "шестьдесят "; break;
+            case 7: cout << "семьдесят "; break;
+            case 8: cout << "восемьдесят "; break;
+            case 9: cout << "девяносто "; break;
+            default:
+                break;
+            }
+            switch (d)
+            {
+            case 1: cout << "один "; break;
+            case 2: cout << "два "; break;
+            case 3: cout << "три "; break;
+            case 4: cout << "четыре "; break;
+            case 5: cout << "пять "; break;
+            case 6: cout << "шесть "; break;
+            case 7: cout << "семь "; break;
+            case 8: cout << "восемь "; break;
+            case 9: cout << "девять "; break;
+            default:
+                break;
+            }
+        }
+        else {
+            switch (z)
+            {
+            case 11: cout << "одиннадцать "; break;
+            case 12: cout << "двенадцать "; break;
+            case 13: cout << "тринадцать "; break;
+            case 14: cout << "четырнадцать "; break;
+            case 15: cout << "пятнадцать "; break;
+            case 16: cout << "шетнадцать "; break;
+            case 17: cout << "семнадцать "; break;
+            case 18: cout << "восемнадцать "; break;
+            case 19: cout << "девятнадцать "; break;
+            default:
+                break;
+            }
+        }
+        
+    }
+    return 0;
+}
