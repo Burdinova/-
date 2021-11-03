9a5fd705e37ae9176f2669a626c35d9acc2a2208
Burdinova <91088809+Burdinova@users.noreply.github.com>
2021-11-03 11:39:02 +0300

    Add files via upload

---
+++ laba 14/5).cpp
@@ -0,0 +1,23 @@
+﻿/*5. Даны целые положительные числа A и B. Найти их наибольший общий делитель (НОД), используя алгоритм Евклида*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int a, b;
+	cout << "Найти наибольший общий делитель (НОД) данных чисел, используя алгоритм Евклида" <<endl;
+	cout << "A=";
+	cin >> a;
+	cout << "B=";
+	cin >> b;
+	while (a != b)
+	{
+		if (a > b) { a = a - b; }
+		else if (b > a) { b = b - a; }
+	}
+
+	cout << a;
+	return 0;
+}
+
